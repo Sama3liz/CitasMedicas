4730353a733200065c39b2e82503c4a0b070c0ae
Sama3liz <aleksander.nakata@gmail.com>
2022-12-19 13:14:26 -0500

    V1.1

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\AdminMiddleware;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
